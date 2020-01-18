1c1a601cc83b7732c6beab3e96d7aafe29abda5f
Bulat Gombotsyrenov <bulawake@gmail.com>
2020-01-18 12:47:19 +0700

    tests for list controller

--- test/awesome_list_web/controllers/list_controller_test.exs
+++ test/awesome_list_web/controllers/list_controller_test.exs
@@ -3,6 +3,20 @@ defmodule AwesomeListWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert true == true
+    assert html_response(conn, 200)
+    assert Map.has_key?(conn.assigns, :stars) == false
   end
+
+  test "GET /?min_stars=50", %{conn: conn} do
+    conn = get conn, "/?min_stars=50"
+    assert html_response(conn, 200)
+    assert conn.assigns.stars == 50
+  end
+
+  test "GET /?min_stars=some_string", %{conn: conn} do
+    conn = get conn, "/?min_stars="
+    assert html_response(conn, 200)
+    assert Map.has_key?(conn.assigns, :stars) == false
+  end
+
 end
