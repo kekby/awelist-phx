2664ec726e88e38378e154929e2e5bbd42fcd2e5
Bulat Gombotsyrenov <bulawake@gmail.com>
2020-01-20 07:03:10 +0700

    md parser improvements

--- lib/awesome_list/modules/github_api.ex
+++ lib/awesome_list/modules/github_api.ex
@@ -2,20 +2,24 @@ defmodule AwesomeList.GithubApi do
   alias AwesomeList.MdParser
 
   def fetch_repo() do
-    get_repo_url()
-    |> HTTPoison.get
+    "awesome_list.md"
+    |> File.read
     |> handle_response
   end
 
-  defp handle_response({ :ok, %{ body: body, status_code: 200 } }) do
-    { :ok, body }
-  end
+  # defp handle_response({ :ok, %{ body: body, status_code: 200 } }) do
+  #   { :ok, body }
+  # end
 
-  defp handle_response({ :ok, %{ status_code: status_code }}) do
-    { :error, "HTTP Status: #{status_code}" }
-  end
+  # defp handle_response({ :ok, %{ status_code: status_code }}) do
+  #   { :error, "HTTP Status: #{status_code}" }
+  # end
 
-  defp handle_response({ :error, %{ reason: reason } }), do: { :error, reason }
+  # defp handle_response({ :error, %{ reason: reason } }), do: { :error, reason }
+
+  defp handle_response({ :ok, raw_string }) do
+    { :ok, raw_string }
+  end
 
   defp get_client() do
     Tentacat.Client.new(%{ access_token: get_access_token() })
--- lib/awesome_list/modules/md_parser.ex
+++ lib/awesome_list/modules/md_parser.ex
@@ -1,6 +1,7 @@
 defmodule AwesomeList.MdParser do
 
-  @category_tag "## "
+  @section_tag  "\n# "
+  @category_tag "\n## "
 
   def parse(body) do
     body
@@ -10,26 +11,41 @@ defmodule AwesomeList.MdParser do
 
   defp get_categories(raw_markdown) do
     raw_markdown
-      |> String.split(@category_tag, trim: true)
-      |> List.delete_at(0)
+      |> clean_markup
+  end
+
+  defp clean_markup(markdown) do
+    markdown
+    |> String.split(@section_tag, trim: true)
+    |> List.first()
+    |> String.split(@category_tag, trim: true)
+    |> List.delete_at(0)
   end
 
   defp parse_categories(category_list) do
     category_list
       |> Task.async_stream(&get_data_from_category(&1))
       |> Enum.to_list
-      |> IO.inspect
   end
 
   defp get_data_from_category(category_raw_string) do
     [title, description | raw_repos ] = category_raw_string
       |> String.split("\n", trim: true)
-    { title, get_description(description), raw_repos }
+    { title, parse_description(description), parse_repos(raw_repos) }
   end
 
-  defp get_description(description) do
+  defp parse_description(description) do
     description
       |> String.replace("*", "")
   end
 
+  defp parse_repos(raw_repos) do
+    raw_repos
+    |> Task.async_stream(&parse_repo(&1))
+    |> Enum.to_list
+  end
+
+  defp parse_repo(repo_data_string) do
+    repo_data_string
+  end
 end
